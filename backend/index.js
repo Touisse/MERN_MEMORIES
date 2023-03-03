7e28955dcc045c77dbe9da9d45f94f8e3a9f24db
Touisse <yassin.touisd@gmail.com>
2023-03-03 12:32:18 +0100

    part on e is done

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,26 @@
-import { express } from "express";
-import bodyParser from "body-parser";
+import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import postRoutes from "./routes/posts.js";
 
+const app = express();
+
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb" }));
+app.use(cors());
+app.use("/posts", postRoutes);
+
+const CONNECTION_URL =
+  "mongodb+srv://yassinetouisse:12345@cluster0.uenejij.mongodb.net/?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`server is running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error));
