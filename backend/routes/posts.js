a55fe7bd5312d6f34cedfe07a8a9d708ee2941b7
Touisse <yassin.touisd@gmail.com>
2023-03-04 20:25:44 +0100

    the backend for search is done

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -5,11 +5,13 @@ import {
   updatePost,
   likePost,
   deletePost,
+  getPostsBySearch
 } from "../controllers/posts.js";
 
 import auth from "../middleware/auth.js";
 const router = express.Router();
 
+router.get("/search", getPostsBySearch);
 router.get("/", getPosts);
 router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
