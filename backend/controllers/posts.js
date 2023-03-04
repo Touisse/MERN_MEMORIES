6d9a890c6b50e4f2e3c8d481bba1ead7c7080f4e
Touisse <yassin.touisd@gmail.com>
2023-03-04 23:34:18 +0100

    the backend i sync

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -6,11 +6,25 @@ import PostMessage from "../models/postMessage.js";
 const router = express.Router();
 
 export const getPosts = async (req, res) => {
+  const { page } = req.query;
+
   try {
-    const postMessages = await PostMessage.find();
-    res.status(200).json(postMessages);
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
+
+    const total = await PostMessage.countDocuments({});
+    const posts = await PostMessage.find()
+      .sort({ _id: -1 })
+      .limit(LIMIT)
+      .skip(startIndex);
+
+    res.json({
+      data: posts,
+      currentPage: Number(page),
+      numberOfPages: Math.ceil(total / LIMIT),
+    });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(404).json({ message: error.message });
   }
 };
 
@@ -19,9 +33,11 @@ export const getPostsBySearch = async (req, res) => {
 
   try {
     const title = new RegExp(searchQuery, "i");
+
     const posts = await PostMessage.find({
       $or: [{ title }, { tags: { $in: tags.split(",") } }],
     });
+
     res.json({ data: posts });
   } catch (error) {
     res.status(404).json({ message: error.message });
